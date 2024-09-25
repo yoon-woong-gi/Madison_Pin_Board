3823df1d5b7070beffedb0cb46639e0d5ebaedcd
Woonggi <woonggi.yoon@gmail.com>
2024-09-25 17:17:37 -0500

    course 14

--- accountapp/models.py
+++ accountapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+# python manage.py makemigrations
+# python manage.py migrate
+
+class HelloWorld(models.Model):
+    text = models.CharField(max_length=255, null=False)
+
