bc74df97e7c68ff21e8790e214384f3431848588
Woonggi <woonggi.yoon@gmail.com>
2024-10-15 11:34:18 -0500

    course 31

--- pragment/urls.py
+++ pragment/urls.py
@@ -19,6 +19,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    # python manage.py createsuperuser 터미널에 입력해서 슈퍼유저를 생성해서 관리
     path('admin/', admin.site.urls),
     path('accounts/', include('accountapp.urls')),
+    path('profiles/', include('profileapp.urls')),
 ]
