b20b065d4db21dc2c5bdc7e08cba00ac5b35d72f
Woonggi <woonggi.yoon@gmail.com>
2024-10-15 20:26:15 -0500

    Ccourse 32

--- pragment/urls.py
+++ pragment/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-
 from django.urls import path, include
 
 urlpatterns = [
@@ -23,4 +24,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accountapp.urls')),
     path('profiles/', include('profileapp.urls')),
-]
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
